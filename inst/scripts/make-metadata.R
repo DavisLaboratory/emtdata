b0d055d07d15a0ff16d8339a869c654c62bb9628
bhuvad <bhuva.d@wehi.edu.au>
2021-03-16 17:24:03 +1100

    Updated metadata for all objects.

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,24 +1,30 @@
 meta = data.frame(
-  Title = c('foroutan2017_se'),
+  Title = c('foroutan2017_se', 'cursons2018_se', 'cursons2015_se'),
   Description = c(
-    'Gene expression data from Foroutan et al., MCR 2017. This gene expression data comes from multiple different studies (microarary and RNA-seq), with cell lines treated using TGFb to induce a mesenchymal shift. Data were combined using SVA and ComBat to remove batch effects.'
+    'Gene expression data from Foroutan et al., MCR 2017. This gene expression data comes from multiple different studies (microarary and RNA-seq), with cell lines treated using TGFb to induce a mesenchymal shift. Data were combined using SVA and ComBat to remove batch effects.',
+    'Gene expression data from Cursons et al., Cell Syst 2018. This gene expression data comes from the human mammary epithelial (HMLE) cell line. A mesenchymal HMLE (mesHMLE) phenotype was induced following treatment with TGFb. The mesHMLE subline was then treated with mir200c to reinduce an epithelial phenotype.',
+    'Gene expression data from Cursons et al., Cell Commun Signal 2015. This gene expression data comes from the PMC42-LA and PMC42-ET cell lines. The former exhibits an epithelial phenotype while the latter is more mesenchymal.'
   ),
   BiocVersion = c(3.13),
   Genome = NA,
   SourceType = c('TXT'),
   SourceUrl = c(
-    'https://doi.org/10.4225/49/5a2a11fa43fe3'
+    'https://doi.org/10.4225/49/5a2a11fa43fe3',
+    'https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJEB25042',
+    'https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA322427'
   ),
   SourceVersion = c('1.0'),
   Species = c('Homo sapiens'),
   TaxonomyId = c(9606),
   Coordinate_1_based = TRUE,
-  DataProvider = c('Walter and Eliza Hall Institute of Medical Research'),
+  DataProvider = c('Walter and Eliza Hall Institute of Medical Research', 'Queensland University of Technology', 'Center for Cancer Biology, Adelaide, Australia'),
   Maintainer = 'Malvika D. Kharbanda <kharbanda.m@wehi.edu.au>',
   RDataClass = 'GSEABase::SummarizedExperiment',
   DispatchClass = 'Rda',
   RDataPath = c(
-    'emtdata/foroutan2017_se.rda'
+    'emtdata/foroutan2017_se.rda',
+    'emtdata/cursons2018_se.rda',
+    'emtdata/cursons2015_se.rda'
   )
 )
 
