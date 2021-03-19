4fa77626caa3ae4f86d48b5f77b7934a45faaa09
malvikakh <malvikakharbanda@gmail.com>
2021-03-19 15:49:36 +1100

    Added description for Cursons2018

--- R/zzz.r
+++ R/zzz.r
@@ -27,12 +27,14 @@ NULL
 
 #' Breast Cancer Cell lines PMC42-ET and PMC42-LA from Cursons et al. (2015)
 #'
-#' Unpublished dataset from the laboratory of Rik Thompson at the Queensland University of Technology (QUT).
-#' Dataset used in Cursons et al. 2015. where parallel ‘deep sequencing’ of RNA (RNA-Seq) were conducted
-#' to examine the changes in expression profiles between PMC42-ET and PMC42-LA cells. Cells were treated
-#' for 3 days in the presence or absence of 10 ng/ml EGF. ONLY the controls are processed.
-#' There are 3 biological replicates per condition which were summed.
-#' Sequenced on the Illumina HiSeq 2000, 100bp paired end.
+#' Unpublished dataset from the laboratory of Rik Thompson at the Queensland
+#' University of Technology (QUT).
+#' Dataset used in Cursons et al. 2015. where parallel ‘deep sequencing’ of RNA
+#' (RNA-Seq) were conducted to examine the changes in expression profiles
+#' between PMC42-ET and PMC42-LA cells. Cells were treated for 3 days in the
+#' presence or absence of 10 ng/ml EGF. ONLY the controls are processed. There
+#' are 3 biological replicates per condition which were summed. Sequenced on the
+#' Illumina HiSeq 2000, 100bp paired end.
 #'
 #' Data from this publication were downloaded from SRA and/or EMBL-EBI ENA and processed into a
 #' SummarizedExperiment object. Sample annotations were modified from the original publication.
@@ -49,5 +51,33 @@ NULL
 #' eh <- ExperimentHub()
 #' emt_datasets <- query(eh, "emtdata")
 #' @name thompson2015_se
+#'
+NULL
+
+#' Combinatorial miRNAs in breast cancer EMT from Cursons et al. (2018)
+#'
+#' Cursons et al. used the Human Mammary Epithelial Cells (HMLE) cell line dataset.
+#' A mesenchymal HMLE (mesHMLE) phenotype was induced following treatment with
+#' transforming growth factors (TGFb). The mesHMLE subline was then treated with
+#' mir200c to reinduce an epithelial phenotype. All mRNA RNA-seq was collected
+#' using the Illumina HiSeq 2500 with a paired end read length of 100bp.
+#'
+#' Data from this publication were downloaded from the European Nucleotide
+#' Archive (ENA) and processed into a SummarizedExperiment object. Sample
+#' annotations were modified from the original publication.
+#'
+#' @format A SummarizedExperiment object, containing gene expression data of
+#'   the Human Mammary Epithelial Cells (HMLE) cell line. The
+#'   [SummarizedExperiment::colData()] function can be used to access the sample
+#'   annotations.
+#' @references Cursons, J., Pillman, K. A., Scheer, K. G., Gregory, P. A.,
+#' Foroutan, M., Hediyeh-Zadeh, S., ... & Davis, M. J. (2018). Combinatorial
+#' targeting by microRNAs co-ordinates post-transcriptional control of EMT. Cell
+#' systems, 7(1), 77-91.
+#' @examples
+#' library(ExperimentHub)
+#' eh <- ExperimentHub()
+#' emt_datasets <- query(eh, "emtdata")
+#' @name Cursons2018_se
 #'
 NULL
