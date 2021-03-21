88c0a650961467941b438f5e2ca0055b60401e0b
Chin Wee Tan <chinwee.tan@gmail.com>
2021-03-22 10:44:36 +1100

    Updated descriptions for Cursons 2015
    Updated to include all treatments and MDA468 cell lines.

--- R/zzz.r
+++ R/zzz.r
@@ -25,32 +25,38 @@
 #'
 NULL
 
-#' Breast Cancer Cell lines PMC42-ET and PMC42-LA from Cursons et al. (2015)
+#' EGF or hypoxia treatment of Breast Cancer Cell lines from Cursons et al. (2015)
 #'
-#' Unpublished dataset from the laboratory of Rik Thompson at the Queensland
-#' University of Technology (QUT).
 #' Dataset used in Cursons et al. 2015. where parallel ‘deep sequencing’ of RNA
-#' (RNA-Seq) were conducted to examine the changes in expression profiles
-#' between PMC42-ET and PMC42-LA cells. Cells were treated for 3 days in the
-#' presence or absence of 10 ng/ml EGF. ONLY the controls are processed. There
-#' are 3 biological replicates per condition which were summed. Sequenced on the
-#' Illumina HiSeq 2000, 100bp paired end.
+#' (RNA-Seq) were conducted to examine the changes in expression profiles between
+#' breast cancer cell lines PMC42-ET, PMC42-LA and MDA-MB-468 cells.
 #'
-#' Data from this publication were downloaded from SRA and/or EMBL-EBI ENA and processed into a
-#' SummarizedExperiment object. Sample annotations were modified from the original publication.
+#' For PMC42 cell lines, cells were treated for 3 or 7 days in the presence or
+#' absence of 10 ng/ml EGF.
+#' For MDA-MB-468 cells, cells were treated for 7 days in the presence or absence
+#' of either 10 ng/ml EGF or kept under Hypoxia.
+#'
+#' There are 3 biological replicates per condition which were summed.
+#' Samples were sequenced on the Illumina HiSeq 2000, 100bp paired end.
+#'
+#' Data from this publication were downloaded from EMBL-EBI ENA and processed into
+#' a SummarizedExperiment object. Sample annotations were modified from the original
+#' publication and SRA portal.
 #'
 #' @format A SummarizedExperiment object, containing gene expression data of
-#'   different sub-lines of human breast cancer cell lines. The [SummarizedExperiment::colData()]
-#'   function can be used to access the sample annotations.
-#' @references Cursons, J., Leuchowius, KJ., Waltham, M., Tomaskovic-Crook, E., Foroutan, M.,
-#' Bracken, CP., Redfern,A., Crampin, EJ., Street, I., Davis, MJ. & Thompson,EW. (2015)
-#' Stimulus-dependent differences in signalling regulate epithelial-mesenchymal plasticity and
-#' change the effects of drugs in breast cancer cell lines. Cell Commun Signal 13, 26 (2015).
+#'   different sub-lines of human breast cancer cell lines. The
+#'   [SummarizedExperiment::colData()] function can be used to access the sample
+#'   annotations.
+#' @references Cursons, J., Leuchowius, KJ., Waltham, M., Tomaskovic-Crook, E.,
+#' Foroutan, M., Bracken, CP., Redfern,A., Crampin, EJ., Street, I., Davis, MJ.
+#' & Thompson,EW. (2015) Stimulus-dependent differences in signalling regulate
+#' epithelial-mesenchymal plasticity and change the effects of drugs in breast
+#' cancer cell lines. Cell Commun Signal 13, 26 (2015).
 #' @examples
 #' library(ExperimentHub)
 #' eh <- ExperimentHub()
 #' emt_datasets <- query(eh, "emtdata")
-#' @name thompson2015_se
+#' @name cursons2015_se
 #'
 NULL
 
